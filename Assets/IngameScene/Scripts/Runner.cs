39c557dc622d40edc2ba63dabeaa9f6d38c053a0
임형진(Hyeong-Jin Lim) <max804@naver.com>
2016-12-10 07:59:42 +0900

    asdf

--- Assets/IngameScene/Scripts/Runner.cs
+++ Assets/IngameScene/Scripts/Runner.cs
@@ -3,57 +3,96 @@ using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Runner : MonoBehaviour {
+
 	protected float maxSpeed = 10.0f;
-	protected float culSpeed = 5.0f;
-	protected float jumpHeight= 250.0f;
-	protected float dJumpHeight= 500.0f;
+	protected float currentSpeed = 5.0f;
+	protected float jumpHeight= 450f;
+	protected float dJumpHeight= 450f;
 	protected bool jumping = false;
 	protected bool dJump = false;
 	protected float accelSpeed= 0.05f;
 	protected int upgrade = 0;
 	protected int right = 1;
 
-	public bool starting = false;
+	public bool canRun = false;
+	public bool isBumped = false;
 
 
-    void Start ()
+	void Start ()
 	{
 		
 	}
-		
+
 	void Update()
 	{
-		Debug.Log (this.GetComponent<Rigidbody2D> ().velocity);
-		//Debug.Log("" + culSpeed);
+		Debug.Log(this.GetComponent<Rigidbody2D>().velocity);
+	}
+		
+	void FixedUpdate()
+	{
+		if(canRun == true && isBumped == false) {
+				runnerRun();
+
+			if(Input.GetKeyDown(KeyCode.Space)) {
+				if(jumping == false)
+					runnerJump();
+				else if(jumping == true && dJump == false)
+					runnerDoubleJump();
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		Debug.Log(coll.relativeVelocity);
+
+		if(coll.gameObject.tag == "floor") {
+			// 점프들 초기화
+			jumping = false;
+			dJump = false;
+		} else if(coll.gameObject.tag == "obstacle") {
+			runnderBumped();
+		}
 	}
 
 	protected void runnerRun()
 	{
 		
-		this.GetComponent<Transform>().Translate(new Vector2 (right, 0) * culSpeed * Time.deltaTime);
-		if (culSpeed < maxSpeed) {
-			culSpeed += accelSpeed;
+		this.transform.Translate(new Vector2 (right, 0) * currentSpeed * Time.deltaTime);
+
+		if (currentSpeed < maxSpeed) {
+			currentSpeed += accelSpeed;
 		} else
-			culSpeed -= accelSpeed*1.5f;
+			currentSpeed -= accelSpeed*1.5f;
 
 	}
-	protected void endOfMap()
-	{
-		right = -1;
-	}
+
 	protected void runnerJump()
 	{
 		this.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * jumpHeight);
 		jumping = true;
-		culSpeed -= 1.0f;
+		currentSpeed -= 1.0f;
 	}
+
 	protected void runnerDoubleJump()
 	{
 		dJump = true;
-		//float vx = this.GetComponent<Rigidbody2D>().velocity.x;
 		this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
 		this.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 0));
 		this.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * dJumpHeight);
+	}
+
+	protected void runnderBumped()
+	{
+		StartCoroutine(Bumbed_c());
+	}
+	private IEnumerator Bumbed_c()
+	{
+		isBumped = true;
+		this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-400f, 400f));
+		yield return new WaitForSeconds(1.5f);
+		isBumped = false;
+
 	}
 
 }
